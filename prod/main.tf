2f8ab6e4e67063ba1c6e28cdff6dd23d79cc7932
dancaps <danny.caperton@gmail.com>
2022-12-05 18:50:06 +0000

    changed the name of the instance due to a failure

--- prod/main.tf
+++ prod/main.tf
@@ -1,7 +1,7 @@
 module "prod" {
   source        = "../module/instance_and_network"
 
-  instance_name = "PROD-instance"
+  instance_name = "p-instance"
   machine_type  = "e2-micro"
   image         = "debian-cloud/debian-11"
   network_name  = "p-terraform-network"
