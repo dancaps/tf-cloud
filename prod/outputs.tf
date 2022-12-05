a3375c798d186c6adb4525072a6a5dc9ba73e6ba
dancaps <danny.caperton@gmail.com>
2022-12-05 18:23:00 +0000

    fixed output path in the outputs.tf file

--- prod/outputs.tf
+++ prod/outputs.tf
@@ -1,19 +1,19 @@
 output "instance_name" {
   description = "The name of the instance"
-  value       = module.dev.instance_name
+  value       = module.prod.instance_name
 }
 
 output "instance_id" {
   description = "The id of the instance"
-  value       = module.dev.instance_id
+  value       = module.prod.instance_id
 }
 
 output "network_name" {
   description = "The name of the network"
-  value       = module.dev.network_name
+  value       = module.prod.network_name
 }
 
 output "network_id" {
   description = "The id of the network"
-  value       = module.dev.network_id
+  value       = module.prod.network_id
 }
